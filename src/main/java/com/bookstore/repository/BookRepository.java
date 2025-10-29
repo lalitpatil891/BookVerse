a2e59ed001db6c403431ab98b8848a386fe76994
lalitpatil891 <lp613408@gmail.com>
2025-10-29 15:28:35 +0530

    initial commit

---
+++ src/main/java/com/bookstore/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package com.bookstore.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bookstore.entity.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer > {
+	
+	// Finds books whose name starts with a specific alphabet (case-insensitive)
+    List<Book> findByNameStartingWithIgnoreCase(String name);
+}
